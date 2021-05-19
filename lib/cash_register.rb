858f7e8d57e8e0ec6489781b2dce108d29efad32
Francisco <fnaveira11@gmail.com>
2021-05-19 18:14:19 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,30 @@
+require 'pry'
+
+class CashRegister
+    attr_accessor :total
+    attr_accessor :discount
+
+    def initialize(total = 0, discount = 20)
+        @total = total
+        @discount = discount
+    end
+
+    def total
+        @total
+    end
+
+    def add_item(title,price,quantity)
+        previous_total = @total
+        new_total = @total + (price * quantity)
+        new_total
+    end
+
+    def apply_discount
+        discount_float= @discount / 100
+        discount_amount= @total * discount_float.to_f
+        @total = @total - discount_amount
+        @total
+    end
+
+end
 
